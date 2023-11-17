b4a989b997b1e93e3a4d91208cf42dc18fcbdbc4
Guilherme Henrique <guilherme.pessoa@academico.ifpb.edu.br>
2023-11-16 21:54:22 -0300

    Adicionando metodos listar e buscar usuario com interação

--- untitled/src/view/Main.java
+++ untitled/src/view/Main.java
@@ -34,6 +34,37 @@ public class Main {
                     JOptionPane.showMessageDialog(null, "Usuário já existe!");
                 }
             }
+            else if (opcao == 1) {
+                UsuarioDao usuarioDao = new UsuarioDao();
+                List<Usuario> lista = usuarioDao.listarUsuarios();
+                for (int i = 0; i < lista.size(); i++){
+                    JOptionPane.showMessageDialog(null, lista.get(i).getEmail());
+                }
+            }
+            else {
+                UsuarioDao usuarioDao = new UsuarioDao();
+                String emailBusca = JOptionPane.showInputDialog(null, "Informe o email:");
+                UsuarioDao usuarioBusca = new UsuarioDao();
+                Usuario retorno = usuarioBusca.buscarPorEmail(emailBusca);
+                if (retorno != null){
+                    JOptionPane.showMessageDialog(null, "Usuário encontrado!");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Usuário não encontrado!");
+                    int yesOrNo = JOptionPane.showConfirmDialog(null, "Deseja adicionar o usuário " +
+                            emailBusca, "Adicionar novo usuário", JOptionPane.YES_NO_OPTION);
+                    if (yesOrNo == 0){
+                        String senha = JOptionPane.showInputDialog(null, "Informe a senha do novo usuário:");
+                        Usuario novoUsuario = new Usuario(emailBusca, senha);
+                        UsuarioDao usuarioAdicionado = new UsuarioDao();
+                        usuarioAdicionado.addUsuario(novoUsuario);
+                        JOptionPane.showMessageDialog(null, "Usuário " + emailBusca + " adicionado!");
+                    }
+                }
+            }
+            opcao = JOptionPane.showOptionDialog(null, "O que deseja fazer:",
+                    "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null,
+                    opcoes, opcoes[0]);
         }
     }
 }
